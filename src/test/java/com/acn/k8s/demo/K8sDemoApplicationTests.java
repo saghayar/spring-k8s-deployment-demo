4adcc27ac4b490670b13a38b81f32bfc5d4b32ec
Samir Aghayarov <samir.aghayarov@accenture.com>
2019-05-31 03:13:35 +0400

    initial commit

---
+++ src/test/java/com/acn/k8s/demo/K8sDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.acn.k8s.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class K8sDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
