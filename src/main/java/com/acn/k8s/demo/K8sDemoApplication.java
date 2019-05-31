068311b6bdae67066bec3c1f83f1bbfd7d1dffa6
Samir Aghayarov <samir.aghayarov@accenture.com>
2019-05-31 04:01:52 +0400

    dummy endpoint added

--- src/main/java/com/acn/k8s/demo/K8sDemoApplication.java
+++ src/main/java/com/acn/k8s/demo/K8sDemoApplication.java
@@ -4,9 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
+@ResponseBody
 public class K8sDemoApplication {
 
     public static void main(String[] args) {
